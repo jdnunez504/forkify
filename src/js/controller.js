373344d5383fcbbfc6817bde959ad0d9a17ff191
Joaquin Nunez <jdnun314@hotmail.com>
2023-04-28 20:39:56 -0500

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,64 @@
+import * as model from './model.js';
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import {loadSearchResults} from "./model.js";
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+    try {
+        const id = window.location.hash.substring(1);
+
+        if (!id) return;
+        recipeView.renderSpinner();
+
+        // 1. Loading recipe
+        await model.loadRecipe(id);
+
+        // 2. Rendering recipe
+        recipeView.render(model.state.recipe);
+    } catch (err) {
+        recipeView.renderError();
+    }
+};
+
+const controlSearchResults = async function () {
+    try {
+        resultsView.renderSpinner();
+
+        // 1) Get search query
+        const query = searchView.getQuery();
+        if(!query) return;
+
+        // 2) Load search results
+        await model.loadSearchResults(query);
+
+        // 3) Render results
+        resultsView.render(model.getSearchResultsPage(2));
+
+        // 4) Render initial pagination buttons
+        paginationView.render(model.state.search);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const controlPagination = function() {
+    console.log('Pag controller')
+}
+
+const init = function() {
+    recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
+};
+
+init();
