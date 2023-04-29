a95ae189bdbf35d895729406e590165937b215a1
Joaquin Nunez <jdnun314@hotmail.com>
2023-04-28 20:36:35 -0500

    first commit

---
+++ src/js/model.js
@@ -0,0 +1,68 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from "./config.js";
+import { getJSON } from "./helpers.js";
+
+export const state = {
+    recipe: {},
+    search: {
+        query: '',
+        results: [],
+        page: 1,
+        resultsPerPage: RES_PER_PAGE
+    }
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}/${id}`);
+
+        const {recipe} = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(state.recipe);
+    } catch (err) {
+        console.error(`${err} 🤯🤯🤯`);
+        throw err;
+    }
+};
+
+export const loadSearchResults = async function(query) {
+    try {
+        state.search.query = query;
+
+        const data = await getJSON(`${API_URL}/?search=${query}`);
+        console.log(data);
+
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                image: rec.image_url,
+                publisher: rec.publisher
+            }
+        });
+
+        console.log(state.search.results);
+
+    } catch (err) {
+        console.error(`${err} 🤯🤯🤯`);
+        throw err;
+    }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage;
+    const end = page * state.search.resultsPerPage;
+
+    return state.search.results.slice(start, end);
+}
